eb3b7f3e7e49de17294a0bf6a712792e4b1ba8d4
Kwangho <khseok1060@gmail.com>
2018-07-19 19:47:00 +0900

    4-6 Creating the Logged Out Navigation

---
+++ screens/LogInScreen/container.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import LogInScreen from "./presenter";
+
+class Container extends Component {
+  render() {
+    return <LogInScreen />;
+  }
+}
+
+export default Container;
